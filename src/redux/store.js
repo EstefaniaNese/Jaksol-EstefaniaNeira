e90c163c6bea9e25c0071e1d83e849fdebff1202
EstefaniaNese <estefanianese@gmail.com>
2024-10-07 03:57:49 -0300

    First Commit

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import machineReducer from './machineSlice';
-
-export const store = configureStore({
-  reducer: {
-    machines: machineReducer,
-  },
-});
